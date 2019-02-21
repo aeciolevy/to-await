0bb2f8cb75840f8e878a91dd532f5e26f77e8259
Alfredo Quintero <alfredoq98@hotmail.com>
2019-02-20 23:01:21 -0600

    Updated example file

--- example/example.js
+++ example/example.js
@@ -1,35 +1,37 @@
-const {to, toAll} = require('../src/index');
+const { to, toAll } = require("../lib/index");
 
 const return10 = () => Promise.resolve(10);
 
-const rejected = () => Promise.reject(new Error('Something wrong'));
+const rejected = () => Promise.reject(new Error("Something wrong"));
 
-const delay = (time) => new Promise((resolve, reject) => setTimeout(() => resolve(50)), time);
+const delay = time =>
+  new Promise((resolve, reject) => setTimeout(() => resolve(50)), time);
 
 const firstPassTest = async () => {
-    try {
-        let [error, result] = await to(return10());
-        console.log(`error: ${error}, result: ${result}`);
+  try {
+    let return10Response = await to(return10());
+    console.log(
+      `error: ${return10Response.error}, result: ${return10Response.data}`
+    );
 
-        [error, result] = await to(delay(3000));
-        console.log(`error: ${error}, result: ${result}`);
-        [error] = await to(rejected());
-        console.log(`error2: ${error}`);
-        if (error) {
-            throw error;
-        }
-    } catch (err) {
-        console.error(err);
+    let delayResponse = await to(delay(3000));
+    console.log(`error: ${delayResponse.error}, result: ${delayResponse.data}`);
+    const { error } = await to(rejected());
+    console.log(`error2: ${error}`);
+    if (error) {
+      throw error;
     }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const secondTest = async () => {
-
-    const arrayOfPromises = array => array.map(async (el, index) => 1 + index);
-    const promises = arrayOfPromises([1, 2, 3, 4, 5, 6, 7]);
-    let [error, data] = await toAll(promises);
-    console.log(`error: ${error}, data: ${data}`);
-}
+  const arrayOfPromises = array => array.map(async (el, index) => 1 + index);
+  const promises = arrayOfPromises([1, 2, 3, 4, 5, 6, 7]);
+  let allResponse = await toAll(promises);
+  console.log(`error: ${allResponse.error}, data: ${allResponse.data}`);
+};
 
 firstPassTest();
 
